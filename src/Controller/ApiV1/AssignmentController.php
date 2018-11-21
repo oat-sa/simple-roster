9d7944f49cf36a72b834e9653372587940e2e9d4
Péter Halász <halaszpeterzsolt@gmail.com>
2018-11-21 12:53:14 +0100

    add swagger doc + add api endpoint skeletons

---
+++ src/Controller/ApiV1/AssignmentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\ApiV1;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/assignments")
+ */
+class AssignmentController
+{
+    /**
+     * @Route("/", name="api_v1_get_assignments", methods={"GET"})
+     */
+    public function getAssignments()
+    {
+        //TODO
+    }
+
+    /**
+     * @Route("/", name="api_v1_add_assignment", methods={"POST"})
+     */
+    public function addAssignment()
+    {
+        //TODO
+    }
+
+    /**
+     * @Route("/{id}/lti-link", name="api_v1_get_assignment_lti_link", methods={"GET"})
+     */
+    public function getAssignmentLtiLink()
+    {
+        //TODO
+    }
+}
