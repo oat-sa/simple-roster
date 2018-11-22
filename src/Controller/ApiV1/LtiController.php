cb1b3b2a72d171880acf968b52c265cd68819f72
Péter Halász <halaszpeterzsolt@gmail.com>
2018-11-22 16:39:32 +0100

    add lti outcome endpoint

---
+++ src/Controller/ApiV1/LtiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\ApiV1;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/lti")
+ */
+class LtiController
+{
+    /**
+     * @Route("/outcome", name="api_v1_lti_outcome", methods={"POST"})
+     */
+    public function outcome()
+    {
+
+    }
+}
