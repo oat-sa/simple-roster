ee746a0df767c71246d48a2c5694b253069f8b0a
Daniel Halasz <danielh@taotesting.com>
2019-07-09 11:37:04 +0200

    Reverted visibility scope of ensureConsoleOutput method in AbstractBulkUsersAssignmentsCommand

--- src/Command/Bulk/AbstractBulkUsersAssignmentsCommand.php
+++ src/Command/Bulk/AbstractBulkUsersAssignmentsCommand.php
@@ -139,7 +139,7 @@ abstract class AbstractBulkUsersAssignmentsCommand extends Command
     /**
      * @throws LogicException
      */
-    private function ensureConsoleOutput(OutputInterface $output): ConsoleOutputInterface
+    protected function ensureConsoleOutput(OutputInterface $output): ConsoleOutputInterface
     {
         if (!$output instanceof ConsoleOutputInterface) {
             throw new LogicException(
