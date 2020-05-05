430261d6866f12a3c3edb13b5aeee599756ea2c9
Cicatrice <cicatrice@cicatrice.eu>
2020-05-05 19:11:56 +0200

    Add dirty way to check code change

--- src/HealthCheck/HealthCheckResult.php
+++ src/HealthCheck/HealthCheckResult.php
@@ -54,6 +54,7 @@ class HealthCheckResult implements JsonSerializable
             'isDoctrineConnectionAvailable' => $this->isDoctrineConnectionAvailable,
             'isDoctrineCacheAvailable' => $this->isDoctrineCacheAvailable,
             'hostname' => $_ENV['HOSTNAME'],
+            'version' => $_ENV['VERSION'],
         ];
     }
 }
