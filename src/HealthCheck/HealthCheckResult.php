df72c7a4d3a02b14ae3e909375ed4ea3270c2974
Cicatrice <cicatrice@cicatrice.eu>
2020-05-19 10:29:36 +0200

    Change version

--- src/HealthCheck/HealthCheckResult.php
+++ src/HealthCheck/HealthCheckResult.php
@@ -54,7 +54,7 @@ class HealthCheckResult implements JsonSerializable
             'isDoctrineConnectionAvailable' => $this->isDoctrineConnectionAvailable,
             'isDoctrineCacheAvailable' => $this->isDoctrineCacheAvailable,
             'hostname' => $_ENV['HOSTNAME'],
-            'version' => $_ENV['VERSION'],
+            'myVersion' => $_ENV['VERSION'],
             'thisIsImportant' => 'GITOPS-99002'
         ];
     }
