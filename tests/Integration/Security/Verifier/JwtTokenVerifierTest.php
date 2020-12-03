92084ca9c4d4e27c40ff49fb37c718cb4c92715a
uncleempty <ilya@taotesting.com>
2020-12-03 12:22:04 +0300

    Added integration test for JWT token verifier

---
+++ tests/Integration/Security/Verifier/JwtTokenVerifierTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OAT\SimpleRoster\Tests\Integration\Security\Verifier;
+
+use OAT\SimpleRoster\Security\Verifier\JwtTokenVerifier;
+use OAT\SimpleRoster\Service\JWT\JWTManager;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Security\Core\User\User;
+
+class JwtTokenVerifierTest extends KernelTestCase
+{
+    public function testItCanVerifyToken(): void
+    {
+        self::bootKernel();
+
+        /** @var JWTManager $tokenManager */
+        $tokenManager = self::$container->get(JWTManager::class);
+
+        $user = new User('username', null);
+
+        $token = $tokenManager->create($user);
+
+        $subject = new JwtTokenVerifier('file://' . __DIR__ . '/../../../../tests/Resources/jwt/public.pem');
+
+        $this->assertTrue($subject->isValid($token));
+    }
+}
