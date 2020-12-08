1351c75240c925217b6951bafdeb419e3653fa99
Daniel Halasz <danielh@taotesting.com>
2020-12-08 19:58:33 +0100

    fix: fixed UpdateLti1p1OutcomeActionTest to read xml fixtures from string

--- tests/Functional/Action/Lti/UpdateLti1p1OutcomeActionTest.php
+++ tests/Functional/Action/Lti/UpdateLti1p1OutcomeActionTest.php
@@ -98,16 +98,6 @@ class UpdateLti1p1OutcomeActionTest extends WebTestCase
             ->method('uuid4')
             ->willReturn('e36f227c-2946-11e8-b467-0ed5f89f718b');
 
-        /** @var string $xmlRequestBody */
-        $xmlRequestBody = file_get_contents(
-            __DIR__ . '/../../../Resources/LtiOutcome/valid_replace_result_body.xml'
-        );
-
-        /** @var string $xmlResponseBody */
-        $xmlResponseBody = file_get_contents(
-            __DIR__ . '/../../../Resources/LtiOutcome/valid_replace_result_response_body.xml'
-        );
-
         $queryParameters = http_build_query([
             'oauth_body_hash' => 'bodyHash',
             'oauth_consumer_key' => $ltiInstance->getLtiKey(),
@@ -126,11 +116,14 @@ class UpdateLti1p1OutcomeActionTest extends WebTestCase
             [
                 'CONTENT_TYPE' => 'text/xml',
             ],
-            $xmlRequestBody
+            $this->getValidReplaceResultXml()
         );
 
         self::assertEquals(Response::HTTP_OK, $this->kernelBrowser->getResponse()->getStatusCode());
-        self::assertEquals($xmlResponseBody, $this->kernelBrowser->getResponse()->getContent());
+        self::assertEquals(
+            $this->getValidReplaceResultResponseXml(),
+            $this->kernelBrowser->getResponse()->getContent()
+        );
         self::assertEquals(Assignment::STATE_READY, $this->getAssignment()->getState());
     }
 
@@ -175,11 +168,6 @@ class UpdateLti1p1OutcomeActionTest extends WebTestCase
         $time = time();
         $signature = $this->generateSignature($ltiInstance, (string)$time);
 
-        /** @var string $xmlBody */
-        $xmlBody = file_get_contents(
-            __DIR__ . '/../../../Resources/LtiOutcome/invalid_replace_result_body_wrong_assignment.xml'
-        );
-
         $queryParameters = http_build_query([
             'oauth_body_hash' => 'bodyHash',
             'oauth_consumer_key' => $ltiInstance->getLtiKey(),
@@ -198,7 +186,7 @@ class UpdateLti1p1OutcomeActionTest extends WebTestCase
             [
                 'CONTENT_TYPE' => 'text/xml',
             ],
-            $xmlBody
+            $this->getInvalidReplaceResultXmlWithWrongAssignment()
         );
 
         self::assertEquals(Response::HTTP_NOT_FOUND, $this->kernelBrowser->getResponse()->getStatusCode());
@@ -252,5 +240,92 @@ class UpdateLti1p1OutcomeActionTest extends WebTestCase
         self::assertInstanceOf(Assignment::class, $assignment);
 
         return $assignment;
+    }
+
+    private function getValidReplaceResultXml(): string
+    {
+        return <<<XML
+<?xml version = "1.0" encoding = "UTF-8"?>
+<imsx_POXEnvelopeRequest xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+    <imsx_POXHeader>
+        <imsx_POXRequestHeaderInfo>
+            <imsx_version>V1.0</imsx_version>
+            <imsx_messageIdentifier>5c592d046d5c6</imsx_messageIdentifier>
+        </imsx_POXRequestHeaderInfo>
+    </imsx_POXHeader>
+    <imsx_POXBody>
+        <replaceResultRequest>
+            <resultRecord>
+                <sourcedGUID>
+                    <sourcedId>1</sourcedId>
+                </sourcedGUID>
+                <result>
+                    <resultScore>
+                        <language>en-us</language>
+                        <textString>0.11111111111111</textString>
+                    </resultScore>
+                </result>
+            </resultRecord>
+        </replaceResultRequest>
+    </imsx_POXBody>
+</imsx_POXEnvelopeRequest>
+XML;
+    }
+
+    private function getValidReplaceResultResponseXml(): string
+    {
+        return <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<imsx_POXEnvelopeResponse xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+    <imsx_POXHeader>
+        <imsx_POXResponseHeaderInfo>
+            <imsx_version>V1.0</imsx_version>
+            <imsx_messageIdentifier>e36f227c-2946-11e8-b467-0ed5f89f718b</imsx_messageIdentifier>
+            <imsx_statusInfo>
+                <imsx_codeMajor>success</imsx_codeMajor>
+                <imsx_severity>status</imsx_severity>
+                <imsx_description>Assignment with Id 1 was updated</imsx_description>
+                <imsx_messageRefIdentifier>1</imsx_messageRefIdentifier>
+                <imsx_operationRefIdentifier>replaceResult</imsx_operationRefIdentifier>
+            </imsx_statusInfo>
+        </imsx_POXResponseHeaderInfo>
+    </imsx_POXHeader>
+    <imsx_POXBody>
+        <replaceResultResponse />
+    </imsx_POXBody>
+</imsx_POXEnvelopeResponse>
+
+XML;
+    }
+
+    private function getInvalidReplaceResultXmlWithWrongAssignment(): string
+    {
+        return <<<XML
+<?xml version = "1.0" encoding = "UTF-8"?>
+<imsx_POXEnvelopeRequest xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+    <imsx_POXHeader>
+        <imsx_POXRequestHeaderInfo>
+            <imsx_version>V1.0</imsx_version>
+            <imsx_messageIdentifier>5c592d046d5c6</imsx_messageIdentifier>
+        </imsx_POXRequestHeaderInfo>
+    </imsx_POXHeader>
+    <imsx_POXBody>
+        <replaceResultRequest>
+            <resultRecord>
+                <sourcedGUID>
+                    <sourcedId>1000000000</sourcedId>
+                </sourcedGUID>
+                <result>
+                    <resultScore>
+                        <language>en-us</language>
+                        <textString>0.11111111111111</textString>
+                    </resultScore>
+                </result>
+            </resultRecord>
+        </replaceResultRequest>
+    </imsx_POXBody>
+</imsx_POXEnvelopeRequest>
+
+XML;
     }
 }
