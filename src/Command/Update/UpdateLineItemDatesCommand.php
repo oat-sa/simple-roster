f0ae3820ad9554bc8b5cd7a3029e3ef179491c3f
Luis Nascimento <luis.nascimento@taotesting.com>
2020-12-09 16:48:28 +0100

    chore: Added added input validators for IDs and slugs

--- src/Command/Update/UpdateLineItemDatesCommand.php
+++ src/Command/Update/UpdateLineItemDatesCommand.php
@@ -22,6 +22,7 @@ declare(strict_types=1);
 
 namespace OAT\SimpleRoster\Command\Update;
 
+use InvalidArgumentException;
 use OAT\SimpleRoster\Command\BlackfireProfilerTrait;
 use OAT\SimpleRoster\Command\Cache\LineItemCacheWarmerCommand;
 use Symfony\Component\Console\Command\Command;
@@ -37,14 +38,23 @@ class UpdateLineItemDatesCommand extends Command
 
     public const NAME = 'roster:update:line-item-dates';
 
-    private const OPTION_IDS = 'ids';
-    private const OPTION_SLUGS = 'slugs';
+    private const OPTION_LINE_ITEM_IDS = 'line-item-ids';
+    private const OPTION_LINE_ITEM_SLUGS = 'line-item-slugs';
     private const OPTION_START_DATE = 'start-date';
     private const OPTION_END_DATE = 'end-date';
     private const OPTION_FORCE = 'force';
 
     /** @var SymfonyStyle */
     private $symfonyStyle;
+
+    /** @var bool */
+    private $isDryRun;
+
+    /** @var string[] */
+    private $lineItemSlugs;
+
+    /** @var int[] */
+    private $lineItemIds;
 
     public function __construct()
     {
@@ -58,38 +68,38 @@ class UpdateLineItemDatesCommand extends Command
         $this->setDescription('Updates the start and end dates of line item(s).');
 
         $this->addOption(
-            self::OPTION_IDS,
+            self::OPTION_LINE_ITEM_IDS,
             'i',
-            InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
-            'Comma separated list of line item IDs to be updated.',
+            InputOption::VALUE_REQUIRED,
+            'Comma separated list of line item IDs to be updated',
         );
 
         $this->addOption(
-            self::OPTION_SLUGS,
+            self::OPTION_LINE_ITEM_SLUGS,
             's',
-            InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
-            'Comma separated list of line item slugs to be updated.',
+            InputOption::VALUE_REQUIRED,
+            'Comma separated list of line item slugs to be updated',
         );
 
         $this->addOption(
             self::OPTION_START_DATE,
             '',
             InputOption::VALUE_REQUIRED,
-            'Start date for the line item(s).',
+            'Start date for the line item(s)',
         );
 
         $this->addOption(
             self::OPTION_END_DATE,
             '',
             InputOption::VALUE_REQUIRED,
-            'End date for the line item(s).',
+            'End date for the line item(s)',
         );
 
         $this->addOption(
             self::OPTION_FORCE,
             'f',
-            InputOption::VALUE_OPTIONAL,
-            'To involve actual database modifications or not.'
+            InputOption::VALUE_NONE,
+            'To involve actual database modifications or not'
         );
     }
 
@@ -100,6 +110,16 @@ class UpdateLineItemDatesCommand extends Command
     {
         $this->symfonyStyle = new SymfonyStyle($input, $output);
         $this->symfonyStyle->title('Simple Roster - Start and End dates update');
+
+        if ($input->getOption(self::OPTION_LINE_ITEM_IDS)) {
+            $this->initializeLineItemIdsOption($input);
+        }
+
+        if ($input->getOption(self::OPTION_LINE_ITEM_SLUGS)) {
+            $this->initializeLineItemSlugsOption($input);
+        }
+
+        $this->isDryRun = !(bool)$input->getOption(self::OPTION_FORCE);
     }
 
     /**
@@ -110,14 +130,53 @@ class UpdateLineItemDatesCommand extends Command
         try {
             $this->symfonyStyle->note('Checking line items to be updated...');
 
-            $command = $this->getApplication()->find(LineItemCacheWarmerCommand::NAME);
-            $command->run($input, $output);
+            dd($this->lineItemIds, $this->isDryRun);
+
+            //$command = $this->getApplication()->find(LineItemCacheWarmerCommand::NAME);
+            //$command->run($input, $output);
 
             return 0;
         } catch (Throwable $exception) {
             $this->symfonyStyle->error(sprintf('An unexpected error occurred: %s', $exception->getMessage()));
 
             return 1;
+        }
+    }
+
+    private function initializeLineItemIdsOption(InputInterface $input): void
+    {
+        $this->lineItemIds = array_filter(
+            explode(',', (string)$input->getOption(self::OPTION_LINE_ITEM_IDS)),
+            static function ($value): bool {
+                return !empty($value) && (int)$value > 0;
+            }
+        );
+
+        if (empty($this->lineItemIds)) {
+            throw new InvalidArgumentException(
+                sprintf("Invalid '%s' option received.", self::OPTION_LINE_ITEM_IDS)
+            );
+        }
+
+        $this->lineItemIds = array_map('intval', $this->lineItemIds);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    private function initializeLineItemSlugsOption(InputInterface $input): void
+    {
+        $this->lineItemSlugs = array_filter(
+            explode(',', (string)$input->getOption(self::OPTION_LINE_ITEM_SLUGS)),
+            static function ($value): bool {
+                return !empty($value) && is_string($value) && mb_strlen($value) > 1;
+            }
+        );
+
+        if (empty($this->lineItemSlugs)) {
+            throw new InvalidArgumentException(
+                sprintf("Invalid '%s' option received.", self::OPTION_LINE_ITEM_SLUGS)
+            );
         }
     }
 }
