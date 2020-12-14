856c7df6eebc3501fe11e6e674f99e222d65a3d7
Andre Machado <andre.machado@taotesting.com>
2020-12-14 09:46:03 +0100

    test: Added new test case for lti link when line item is deactivated

--- tests/Functional/Action/Lti/GetUserAssignmentLtiLinkActionTest.php
+++ tests/Functional/Action/Lti/GetUserAssignmentLtiLinkActionTest.php
@@ -27,10 +27,12 @@ use DateInterval;
 use DateTimeZone;
 use Monolog\Logger;
 use OAT\SimpleRoster\Entity\Assignment;
+use OAT\SimpleRoster\Entity\LineItem;
 use OAT\SimpleRoster\Entity\User;
 use OAT\SimpleRoster\Generator\NonceGenerator;
 use OAT\SimpleRoster\Lti\LoadBalancer\LtiInstanceLoadBalancerFactory;
 use OAT\SimpleRoster\Lti\Request\LtiRequest;
+use OAT\SimpleRoster\Repository\LineItemRepository;
 use OAT\SimpleRoster\Repository\UserRepository;
 use OAT\SimpleRoster\Security\OAuth\OAuthContext;
 use OAT\SimpleRoster\Tests\Traits\DatabaseTestingTrait;
@@ -72,7 +74,10 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
     {
         $this->kernelBrowser->request('POST', '/api/v1/assignments/1/lti-link');
 
-        self::assertSame(Response::HTTP_METHOD_NOT_ALLOWED, $this->kernelBrowser->getResponse()->getStatusCode());
+        self::assertSame(
+            Response::HTTP_METHOD_NOT_ALLOWED,
+            $this->kernelBrowser->getResponse()->getStatusCode()
+        );
     }
 
     public function testItReturns404IfAssignmentDoesNotBelongToAuthenticatedUser(): void
@@ -94,7 +99,10 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
             JSON_THROW_ON_ERROR
         );
 
-        self::assertSame("Assignment id '2' not found for user 'user1'.", $decodedResponse['error']['message']);
+        self::assertSame(
+            "Assignment id '2' not found for user 'user1'.",
+            $decodedResponse['error']['message']
+        );
     }
 
     public function testItReturns409IfAssignmentDoesNotHaveASuitableState(): void
@@ -150,6 +158,43 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
         Carbon::setTestNow();
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    public function testItReturns409IfLineItemIsNotAvailable(): void
+    {
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
+
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->getRepository(User::class);
+        $user = $userRepository->findByUsernameWithAssignments('user1');
+
+        /** @var LineItemRepository $lineItemRepository */
+        $lineItemRepository = $this->getRepository(LineItem::class);
+        $lineItemRepository->findOneById(1)
+            ->deactivate();
+        $lineItemRepository->flush();
+
+        $this->logInAs($user, $this->kernelBrowser);
+
+        $_ENV['LTI_VERSION'] = LtiRequest::LTI_VERSION_1P1;
+        $_ENV['LTI_INSTANCE_LOAD_BALANCING_STRATEGY'] = LtiInstanceLoadBalancerFactory::STRATEGY_USERNAME;
+
+        $this->kernelBrowser->request('GET', '/api/v1/assignments/1/lti-link');
+
+        self::assertSame(Response::HTTP_CONFLICT, $this->kernelBrowser->getResponse()->getStatusCode());
+
+        $decodedResponse = json_decode($this->kernelBrowser->getResponse()->getContent(), true);
+        self::assertSame(
+            "Assignment with id '1' for user 'user1' is unavailable.",
+            $decodedResponse['error']['message']
+        );
+
+        Carbon::setTestNow();
+    }
+
     public function testItReturns409IfAssignmentHasReachedMaximumAttempts(): void
     {
         $this->loadFixtureByFilename('userWithAllAttemptsTaken.yml');
@@ -176,7 +221,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
      */
     public function testItReturnsLtiLinkAndUpdatedAssignmentAndAttemptsCountWithUsernameLoadBalancerStrategy(): void
     {
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -217,7 +264,7 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
                     'launch_presentation_locale' => 'en-EN',
                 ],
             ],
-            json_decode($this->kernelBrowser->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR)
+            json_decode($this->kernelBrowser->getResponse()->getContent(), true)
         );
 
         /** @var Assignment $assignment */
@@ -233,7 +280,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
      */
     public function testItReturnsLtiLinkAndUpdatedAssignmentAndAttemptsCountWithUserGroupIdLoadBalancerStrategy(): void
     {
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -274,7 +323,7 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
                     'launch_presentation_locale' => 'en-EN',
                 ],
             ],
-            json_decode($this->kernelBrowser->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR)
+            json_decode($this->kernelBrowser->getResponse()->getContent(), true)
         );
 
         /** @var Assignment $assignment */
@@ -296,7 +345,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
         $_ENV['LTI_LAUNCH_PRESENTATION_LOCALE'] = 'it-IT';
         $_ENV['LTI_INSTANCE_LOAD_BALANCING_STRATEGY'] = LtiInstanceLoadBalancerFactory::STRATEGY_USERNAME;
 
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -336,7 +387,7 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
                     'launch_presentation_locale' => 'it-IT',
                 ],
             ],
-            json_decode($this->kernelBrowser->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR)
+            json_decode($this->kernelBrowser->getResponse()->getContent(), true)
         );
 
         /** @var Assignment $assignment */
@@ -352,7 +403,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
 
         $this->getEntityManager()->flush();
 
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -374,7 +427,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
 
     public function testItLogsSuccessfulLtiRequestCreation(): void
     {
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -415,7 +470,9 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
      */
     public function testItReturnsLti1p3Link(): void
     {
-        Carbon::setTestNow(Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC')));
+        Carbon::setTestNow(
+            Carbon::create(2019, 1, 1, 0, 0, 0, new DateTimeZone('UTC'))
+        );
 
         /** @var UserRepository $userRepository */
         $userRepository = $this->getRepository(User::class);
@@ -429,7 +486,12 @@ class GetUserAssignmentLtiLinkActionTest extends WebTestCase
 
         self::assertSame(Response::HTTP_OK, $this->kernelBrowser->getResponse()->getStatusCode());
 
-        $response = json_decode($this->kernelBrowser->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $response = json_decode(
+            $this->kernelBrowser->getResponse()->getContent(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
         $decodedLtiLink = urldecode($response['ltiLink']);
 
         self::assertStringContainsString('iss=https://simple-roster.localhost/platform', $decodedLtiLink);
