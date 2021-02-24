0e400428819f8f8fa9f7c5e527f9abcd6e32438f
Daniel Halasz <danielh@taotesting.com>
2021-02-24 11:06:53 +0100

    fix: cr fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -24,6 +24,4 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
