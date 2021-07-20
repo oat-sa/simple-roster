919f3919a54f79edc186d7fd143e0a9d64dea5e0
Luis Nascimento <luis.nascimento@taotesting.com>
2021-07-20 23:39:18 +0200

    chore: added new fields for finding criterias

--- src/Repository/Criteria/FindLineItemCriteria.php
+++ src/Repository/Criteria/FindLineItemCriteria.php
@@ -22,6 +22,8 @@ declare(strict_types=1);
 
 namespace OAT\SimpleRoster\Repository\Criteria;
 
+use DateTimeInterface;
+
 class FindLineItemCriteria
 {
     /** @var int[] */
@@ -29,6 +31,15 @@ class FindLineItemCriteria
 
     /** @var string[] */
     private array $lineItemSlugs = [];
+
+    /** @var string[] */
+    private array $lineItemLabels = [];
+
+    /** @var string[] */
+    private array $lineItemUris = [];
+
+    private DateTimeInterface $lineItemStartAt;
+    private DateTimeInterface $lineItemEndtAt;
 
     public function addLineItemIds(int ...$lineItemIds): self
     {
@@ -44,6 +55,34 @@ class FindLineItemCriteria
         return $this;
     }
 
+    public function addLineItemLabels(string ...$lineItemLabels): self
+    {
+        $this->lineItemLabels = $lineItemLabels;
+
+        return $this;
+    }
+
+    public function addLineItemUris(string ...$lineItemUris): self
+    {
+        $this->lineItemUris = $lineItemUris;
+
+        return $this;
+    }
+
+    public function addLineItemStartAt(DateTimeInterface $lineItemStartAt): self
+    {
+        $this->lineItemStartAt = $lineItemStartAt;
+
+        return $this;
+    }
+
+    public function addLineItemEndAt(DateTimeInterface $lineItemEndAt): self
+    {
+        $this->lineItemEndtAt = $lineItemEndAt;
+
+        return $this;
+    }
+
     public function getLineItemIds(): array
     {
         return $this->lineItemIds;
@@ -54,6 +93,26 @@ class FindLineItemCriteria
         return $this->lineItemSlugs;
     }
 
+    public function getLineItemLabels(): array
+    {
+        return $this->lineItemLabels;
+    }
+
+    public function getLineItemUris(): array
+    {
+        return $this->lineItemUris;
+    }
+
+    public function getLineItemStartAt(): DateTimeInterface
+    {
+        return $this->lineItemStartAt;
+    }
+
+    public function getLineItemEndAt(): DateTimeInterface
+    {
+        return $this->lineItemEndtAt;
+    }
+
     public function hasLineItemIdsCriteria(): bool
     {
         return !empty($this->lineItemIds);
@@ -62,5 +121,25 @@ class FindLineItemCriteria
     public function hasLineItemSlugsCriteria(): bool
     {
         return !empty($this->lineItemSlugs);
+    }
+
+    public function hasLineItemLabelsCriteria(): bool
+    {
+        return !empty($this->lineItemLabels);
+    }
+
+    public function hasLineItemUrisCriteria(): bool
+    {
+        return !empty($this->lineItemUris);
+    }
+
+    public function hasLineItemStartAt(): bool
+    {
+        return !empty($this->lineItemStartAt);
+    }
+
+    public function hasLineItemEndAt(): bool
+    {
+        return !empty($this->lineItemEndAt);
     }
 }
