aa756c9be92e1084f56feb5ebf5f06e27f476713
Sergii Chernenko <sergii@taotesting.com>
2026-03-11 12:16:28 +0100

    chore: alignment

--- docker/phpfpm/build.sh
+++ docker/phpfpm/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 
 cd /var/www/html
-openssl genpkey -aes-256-cbc -algorithm RSA -pass pass:devpassphrase -out config/secrets/docker/jwt_private.pem
-openssl pkey -in config/secrets/docker/jwt_private.pem -passin pass:devpassphrase -out config/secrets/docker/jwt_public.pem -pubout
 composer install
